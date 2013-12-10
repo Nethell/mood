b0f35f5769dd0c9b2e1b60296ca4ae764ba70c92
Raven Lung <wlwang41@gmail.com>
2013-12-10 14:54:09 +0800

    add structure of mood

---
+++ mood/consts.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""This module provides some consts of this project such as mongo port etc.
+"""
---
+++ mood/tools.py
@@ -0,0 +1,4 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""This module provides some public function that will treated as utils.
+"""
