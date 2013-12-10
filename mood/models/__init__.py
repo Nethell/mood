ad5f4c670d9762952e02db088306b02866dcb203
Raven Lung <crowwang@sohu-inc.com>
2013-12-10 17:24:59 +0800

    add models

--- mood/models/__init__.py
+++ mood/models/__init__.py
@@ -1,2 +1,23 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+#                            _    __
+#                           | |  / _|
+#  _ __ ___   ___   ___   __| | | |_ _ __ ___
+# | '_ ` _ \ / _ \ / _ \ / _` | |  _| '_ ` _ \
+# | | | | | | (_) | (_) | (_| |_| | | | | | | |
+# |_| |_| |_|\___/ \___/ \__,_(_)_| |_| |_| |_|
+
+"""This is the base class of models."""
+
+from mongokit import Connection, Document
+from mood import settings
+
+
+conn = Connection(settings.MONGO_HOST, port=settings.MONGO_PORT)
+
+
+class BaseDoc(Document):
+    __database__ = 'mood'
+
+# NOTE: Don't change its position unless you do know what you do.
+from mood.models import comment, scenery, story, user
--- mood/settings.py
+++ mood/settings.py
@@ -1,2 +1,7 @@
-#!/usr/bin/env python
 # -*- coding: utf-8 -*-
+
+#NOTE: should be overwriten by the local setting
+MONGO_HOST = "127.0.0.1"
+MONGO_PORT = 27017
+MONGO_LOG_PATH = "/usr/local/var/log/mongodb/mongo.log"
+MONGO_DB_PATH = "/usr/local/var/log/mongodb"
