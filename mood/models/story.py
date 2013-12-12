a90e13f16fbd3796fc9d82f36e4ef70df9c67a41
linusp <linusp1024@gmail.com>
2013-12-12 21:32:59 +0800

    Define the model of 'story'

--- mood/models/story.py
+++ mood/models/story.py
@@ -6,3 +6,36 @@
 # | '_ ` _ \ / _ \ / _ \ / _` | |  _| '_ ` _ \
 # | | | | | | (_) | (_) | (_| |_| | | | | | | |
 # |_| |_| |_|\___/ \___/ \__,_(_)_| |_| |_| |_|
+
+"""The model of stories"""
+
+import datetime
+from bson import ObjectId
+from mood.models import conn
+from mood.models import BaseDoc
+
+
+@conn.register
+class Story(BaseDoc):
+    """Document of a users' stories
+    Fields:
+    - user: The one who posts this story
+    - scenery: The scenery of this story
+    - tags: Tags attached on this story
+    - content: The content of this story
+    - post_time: The time when this story be posted
+    """
+
+    __collection__ = "stories"
+
+    structure = {
+        'user': ObjectId,
+        'scenery': ObjectId,
+        'tags': [basestring],
+        'content': basestring,
+        'post_time': datetime.datetime
+    }
+
+    required = ['user', 'scenery', 'tags', 'content', 'post_time']
+
+    use_autorefs = True
